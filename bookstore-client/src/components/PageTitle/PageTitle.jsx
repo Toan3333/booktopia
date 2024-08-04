784f83b68f3df002f41d61fec376007db0dad532
Anh Toan <toan21420@gmail.com>
2024-08-04 16:54:36 +0700

    Upload source code

--- bookstore-client/src/components/PageTitle/PageTitle.jsx
+++ bookstore-client/src/components/PageTitle/PageTitle.jsx
@@ -4,7 +4,7 @@ const PageTitle = ({ title, className = "" }) => {
   return (
     <div>
       <h2
-        className={`text-textTitle font-semibold leading-normal text-black ${className} max-md:text-[16px] max-md:leading-6`}>
+        className={`text-textTitle font-semibold leading-normal text-black ${className} max-md:text-[16px] max-md:leading-6 max-lg:text-2xl`}>
         {title}
       </h2>
     </div>
