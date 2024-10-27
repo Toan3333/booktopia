6cbc089cefcb036461b82e77ffbb92a524c0f2cf
Anh Toan <toan21420@gmail.com>
2024-10-28 00:01:39 +0700

    upload code

--- bookstore-client/src/components/Product/ProductViews.jsx
+++ bookstore-client/src/components/Product/ProductViews.jsx
@@ -5,8 +5,18 @@ const ProductViews = ({ views }) => {
   return (
     <div className="flex items-center gap-1 text-sm text-gray-500">
       <FaEye className="w-4 h-4" />
-      <span>{views !== undefined ? views : '0'}</span>
+      <span>{views !== undefined ? views : "0"}</span>
     </div>
+
+    // điều kiện view lớn hơn 100 mới cho hiện
+    // <>
+    //   {views > 100 && (
+    //     <div className="flex items-center gap-1 text-sm text-gray-500">
+    //       <FaEye className="w-4 h-4" />
+    //       <span>{views !== undefined ? views : '0'}</span>
+    //     </div>
+    //   )}
+    // </>
   );
 };
 
