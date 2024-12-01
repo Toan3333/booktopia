98cdc7936cf52660171009eb43232b3f26bb40b6
Anh Toan <toan21420@gmail.com>
2024-12-01 21:57:20 +0700

    Upload code

--- bookstore-client/src/components/Comment/CommentItem.jsx
+++ bookstore-client/src/components/Comment/CommentItem.jsx
@@ -20,10 +20,7 @@ const CommentItem = ({ dataComent, handleDeleteComment }) => {
       <div className="">
         {dataComent && dataComent.length > 0 ? (
           dataComent.map((comment) => (
-            <div
-              key={comment._id}
-              className="flex items-center  mb-2 justify-between"
-            >
+            <div key={comment._id} className="flex items-center  mb-2 justify-between">
               <div className="w-[100%]">
                 <div className="border flex justify-between  rounded-[10px] py-4 px-5 w-full">
                   <div className="flex flex-col gap-2">
@@ -38,32 +35,31 @@ const CommentItem = ({ dataComent, handleDeleteComment }) => {
                           className="w-10 h-10 rounded-full"
                           alt={comment?.user?.name || "User Avatar"}
                         />
-                        <h3 className="text-[18px] text-text font-semibold leading-normal">
-                          {comment?.user?.name}
-                        </h3>
-                        <span className="text-[12px]">{new Date(comment?.day).toLocaleString("vi-VN", {
-                          day: "2-digit",
-                          month: "2-digit",
-                          year: "numeric",
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        })}</span>
+                        <div className="flex flex-col">
+                          <h3 className="text-[18px] text-text font-semibold leading-normal">
+                            {comment?.user?.name}
+                          </h3>
+                          <span className="text-[12px]">
+                            {new Date(comment?.day).toLocaleString("vi-VN", {
+                              day: "2-digit",
+                              month: "2-digit",
+                              year: "numeric",
+                              hour: "2-digit",
+                              minute: "2-digit",
+                            })}
+                          </span>
+                        </div>
                       </div>
-                      
                     </div>
-                    <p className="text-text font-normal leading-normal">
-                      {comment?.content}
-                    </p>
+                    <p className="text-text font-normal leading-normal">{comment?.content}</p>
                   </div>
                   {comment?.user?._id === inforUser?._id ? (
-                <div className="cursor-pointer font-medium">
-                    <AiOutlineDelete onClick={() => handleDeleteComment(comment?._id)} />
+                    <div className="cursor-pointer font-medium">
+                      <AiOutlineDelete onClick={() => handleDeleteComment(comment?._id)} />
+                    </div>
+                  ) : null}
                 </div>
-              ) : null}
-                </div>
-               
               </div>
-              
             </div>
           ))
         ) : (
