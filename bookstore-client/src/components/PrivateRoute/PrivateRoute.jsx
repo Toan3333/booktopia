2eb6e25947ac23b321211b576685a1a61b5bda67
Anh Toan <toan21420@gmail.com>
2024-12-12 00:19:02 +0700

    Upload code

--- bookstore-client/src/components/PrivateRoute/PrivateRoute.jsx
+++ bookstore-client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,28 +1,3 @@
-// import { Navigate } from "react-router-dom";
-// import Cookies from "js-cookie";
-
-// const PrivateRoute = ({ children, allowedRoles }) => {
-//   try {
-//     const userCookie = Cookies.get("user");
-//     const user = userCookie ? JSON.parse(userCookie) : null;
-//     if (!user) {
-//       return <Navigate to="/sign-in" replace />;
-//     }
-//     // profile thì role phải bằng 0
-//     if (allowedRoles.includes(0) && user.user.role !== 0) {
-//       return <Navigate to="/sign-in" replace />;
-//     }
-//     if (allowedRoles.includes(1) && user.user.role !== 1) {
-//       return <Navigate to="/sign-in" replace />;
-//     }
-//     return children;
-//   } catch (error) {
-//     console.error("Lỗi khi đọc cookie:", error);
-//     return <Navigate to="/sign-in" replace />;
-//   }
-// };
-
-// export default PrivateRoute;
 import { Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
@@ -30,14 +5,17 @@ const PrivateRoute = ({ children, allowedRoles }) => {
   try {
     const userCookie = Cookies.get("user");
     const user = userCookie ? JSON.parse(userCookie) : null;
-    const userRole = user ? user.user.role : null; //role từ user
+    const userRole = user ? user.user.role : null; // role từ user
 
-    // // Kiểm tra role
-    if (allowedRoles.includes(userRole) || (!user && allowedRoles.includes(0))) {
-      return children; // Cho phép truy cập
+    if (
+      (user && allowedRoles.includes(-1) && userRole !== 1) || // Đã đăng nhập nhưng không phải admin
+      allowedRoles.includes(userRole) ||
+      (!user && allowedRoles.includes(0))
+    ) {
+      return children;
     }
 
-    //về trang đăng nhập, không có quyền
+    // Chuyển hướng về trang đăng nhập
     return <Navigate to="/sign-in" replace />;
   } catch (error) {
     console.error("Lỗi khi đọc cookie:", error);
