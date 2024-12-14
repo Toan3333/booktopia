df449a5a52e21b52cb8228990088ecccb71deb45
Lê Minh Tú <leminhtu10062004@gmail.com>
2024-12-14 20:28:05 +0700

    upload source code

--- bookstore-client/src/pages/Admin/Dashboard/DashBoard.jsx
+++ bookstore-client/src/pages/Admin/Dashboard/DashBoard.jsx
@@ -246,15 +246,6 @@ const DashBoard = () => {
                 Dashboard
               </div>
             </MenuItem>
-
-            <SubMenu
-              label="Quản lý danh mục"
-              icon={<AiOutlineBars className="w-5 h-5" />}
-            >
-              <MenuItem component={<Link to="/admin/manage-category" />}>
-                Danh sách danh mục
-              </MenuItem>
-            </SubMenu>
             <SubMenu
               label="Quản lý sản phẩm"
               icon={<FaBook className="w-5 h-5" />}
@@ -269,6 +260,13 @@ const DashBoard = () => {
                 Nhà xuất bản
               </MenuItem>
             </SubMenu>
+            <MenuItem component={<Link to="/admin/manage-category" />}>
+              <div className="flex items-center gap-4">
+              <AiOutlineBars className="w-5 h-5" />
+                Quản lý danh mục
+              </div>
+            </MenuItem>
+            
             <MenuItem component={<Link to="/admin/manage-order" />}>
               <div className="flex items-center gap-4">
                 <FaClipboardList className="w-5 h-5" />
@@ -287,14 +285,12 @@ const DashBoard = () => {
                 Quản lý voucher
               </div>
             </MenuItem>
-            <SubMenu
-              label="Quản lý bài viết"
-              icon={<FaRegEdit className="w-5 h-5" />}
-            >
-              <MenuItem component={<Link to="/admin/manage-blog" />}>
-                Danh sách bài viết
-              </MenuItem>
-            </SubMenu>
+            <MenuItem component={<Link to="/admin/manage-blog" />}>
+              <div className="flex items-center gap-4">
+              <FaRegEdit className="w-5 h-5" />
+                Quản lý bài viết
+              </div>
+            </MenuItem>
             <MenuItem component={<Link to="/admin/manage-contact" />}>
               <div className="flex items-center gap-4">
                 <MdMarkEmailRead />
